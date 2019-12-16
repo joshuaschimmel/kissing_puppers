691b3570cef0277e86258f9fc85298e0de8f2e47
joshuaschimmel <joshuaschimmelpfennig@gmail.com>
2019-12-16 15:10:08 +0100

    fixed, commented and express setup

--- backend/app.js
+++ backend/app.js
@@ -1,39 +1,47 @@
-// Modules from the tutorial
+/**
+ * Dependencies
+ */
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const mongoose = require('mongoose');
-
 const fs = require('fs');
 
-// setup routes
-const indexRouter = require('./routes/index');
+const apiRouter = require('./routes/api');
 
 
 let app = express();
 
-// setup database
+/**
+ * Setup database
+ */
 const data_json = JSON.parse(
-    fs.readFileSync(__dirname + 'data/response5.json', 'utf8')
+    fs.readFileSync(__dirname + '/data/response5.json', 'utf8')
 );
-data_json.on('error', console.error.bind(console, 'DB error: '));
+// later for the actual db
+// data_json.on('error', console.error.bind(console, 'DB error: '));
 
-// setup express
+/**
+ * Setup middleware and routing
+ */
 app.use(logger('dev'));
-app.use(express.json);
+app.use(express.json());
 app.use(express.urlencoded({extended: false}));
-app.use(express.static(path.join(__dirname, '../frontend/public')));
+app.use(cookieParser());
 
-app.use('/', indexRouter);
+// use static file for index page
+app.use(express.static(path.join(__dirname, '../frontend/build')));
+// app.use('/', indexRouter);
+app.use('/api', apiRouter)
 
 // catch all other routes as 404
 app.use(function(req, res, next){
     next(createError(404));
 });
 
-// error handler
+// catch and handle errors thrown in middleware functions (next(err))
 app.use(function(err, req, res, next) {
     // set locals, only providing error in develoment
     res.locals.message = err.message;
@@ -47,7 +55,6 @@ app.use(function(err, req, res, next) {
 module.exports = app;
 
 /*
-
 const server = http.createServer((req, res) => {
     console.log(`request was made: ${req.url} with method ${req.method}`);
     let body = [];
