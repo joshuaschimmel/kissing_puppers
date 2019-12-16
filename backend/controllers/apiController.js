0df9d8db7d5ceaa6a1736969131df7817c104aa3
joshuaschimmel <joshuaschimmelpfennig@gmail.com>
2019-12-16 15:09:19 +0100

    added skeleton code for api controller

--- backend/controllers/apiController.js
+++ backend/controllers/apiController.js
@@ -1,11 +1,18 @@
 const Pupper = require('../models/pupper');
 
-// Return the next pupper
+// TODO index page of api?
+exports.index = function(req, res, next) {
+    let err = new Error('No index page for api found');
+    err.status = 404;
+    next(err);
+}
+
+// Return the next pupper (use cookie?)
 exports.next_pupper = function(req, res) {
-    res.send('NOT IMPLEMENTED!');
+    res.json({status: "Not implemented!"});
 };
 
 // Return a random pupper
 exports.random_pupper = function(req, res) {
-    res.send('NOT IMPLEMENTED');
+    res.json({status: "Not implemented!"});
 };
