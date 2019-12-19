fa9d00602dc9c7fb171f848e574af9154c88b256
joshuaschimmel <joshuaschimmelpfennig@gmail.com>
2019-12-19 20:39:12 +0100

    fixed apiController

--- backend/controllers/apiController.js
+++ backend/controllers/apiController.js
@@ -2,7 +2,7 @@ const Pupper = require('../models/pupper');
 
 // TODO index page of api?
 exports.index = function(req, res, next) {
-    let err = new Error('No index page for api found');
+    let err = new Error('No index page for api');
     err.status = 404;
     next(err);
 }
@@ -15,18 +15,19 @@ exports.next_pupper = function(req, res) {
 // Return a random pupper
 exports.random_pupper = function(req, res, next) {
     Pupper.countDocuments().exec(function(err, count){
+        
         if(err) {return next(err);}
         const random = Math.floor(Math.random() * count);
         
         Pupper.findOne().skip(random).exec(
             function(err, result){
                 if(err) {return next(err);}
-                if(result === null) {
+                if(result == null) {
                     error = new Error("Resource not found!");
                     error.status = 404;
                     return next(err);
                 }
-                res.json(result);
+                res.json(result.json_datum);
             });
     });
     
